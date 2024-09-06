46988b53bd57400ada690ad9c0d4fbcdc0a40090
Adriel-Castaneda0565 <164534487+Adriel-Castaneda0565@users.noreply.github.com>
2024-09-06 12:02:04 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,4 @@
+panas = {"kevin", "cass", "melany","roberto"}
+
+for x in panas:
+  print(x)
---
+++ diccionario.py
@@ -0,0 +1,7 @@
+dinos =	{
+  "dino": "Rex",
+  "color": "cafe",
+  "estatura": 16
+}
+for x in dinos:
+  print(x)
